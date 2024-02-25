acbca0cea19a5a44d4aa08ac7bdb85c1e5df51fd
Miroslav Doan <Slithercze@gmail.com>
2024-02-25 20:14:50 +0100

    inner class challenge

--- src/nestedClasses/burger/Meal.java
+++ src/nestedClasses/burger/Meal.java
@@ -1,35 +1,42 @@
 package nestedClasses.burger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Meal {
 
     private double price = 5.0;
-    private Item burger;
+    private Burger burger;
     private Item drink;
     private Item side;
 
     private double conversionRate;
 
-    public Meal(){
+    public Meal() {
         this(1);
     }
 
     public Meal(double conversionRate) {
         this.conversionRate = conversionRate;
-        burger = new Item("regular", "burger");
+        burger = new Burger("regular");
         drink = new Item("coke", "drink", 1.5);
         System.out.println(drink.name);
         side = new Item("fries", "side", 2.0);
     }
 
-    public double getTotal(){
-        double total = burger.price + drink.price + side.price;
-        return Item.getPrice(total,conversionRate);
+    public double getTotal() {
+        double total = burger.getPrice() + drink.price + side.price;
+        return Item.getPrice(total, conversionRate);
     }
 
     @Override
     public String toString() {
         return "%s%n%s%n%s%n%26s$%.2f".formatted(burger, drink, side,
-                "Total Due: ",getTotal());
+                "Total Due: ", getTotal());
+    }
+
+    public void addToppings(String... selectedToppings){
+        burger.addToppings(selectedToppings);
     }
 
     private class Item {
@@ -50,11 +57,61 @@ public class Meal {
 
         @Override
         public String toString() {
-            return "%10s%15s $%.2f".formatted(type, name, getPrice(price,conversionRate));
+            return "%10s%15s $%.2f".formatted(type, name, getPrice(price, conversionRate));
         }
 
         private static double getPrice(double price, double rate) {
             return price * rate;
+        }
+    }
+
+    private class Burger extends Item {
+
+        private enum Extra {AVOCADO, BACON, CHEESE, KETCHUP, MAYO, MUSTARD, PICKLES; //default je static, adding private so we can use it only in burger class
+
+            private double getPrice(){
+                return switch (this){
+                    case AVOCADO -> 1.0;
+                    case BACON,CHEESE -> 1.5;
+                    default -> 0;
+                };
+            }
+        }
+
+        private List<Item> toppings = new ArrayList<>();
+
+        Burger(String name) {
+            super(name, "burger", 5.0);
+        }
+
+        public double getPrice() {
+            double total = super.price;
+            for (Item topping : toppings){
+                total += topping.price;
+            }
+            return total;
+        }
+
+        private void addToppings(String... selectedToppings){
+
+            for (String selectedTopping : selectedToppings){
+                try {
+                    Extra topping = Extra.valueOf(selectedTopping.toUpperCase());
+                    toppings.add(new Item(topping.name(), "TOPPING", topping.getPrice()));
+                } catch (IllegalArgumentException ie){
+                    System.out.println("No topping found for " + selectedTopping);
+                }
+            }
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder itemized = new StringBuilder(super.toString());
+            for (Item topping : toppings){
+                itemized.append("\n");
+                itemized.append(topping);
+            }
+            return itemized.toString();
         }
     }
 }
