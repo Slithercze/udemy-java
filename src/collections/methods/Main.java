6d5da52c6032500b57cfc9210fd417c5768f5220
Miroslav Doan <Slithercze@gmail.com>
2024-03-02 01:30:14 +0100

    code setup for collections

---
+++ src/collections/methods/Main.java
@@ -0,0 +1,11 @@
+package collections.methods;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Card> deck = Card.getStandardDeck();
+        Card.printDeck(deck);
+    }
+}
