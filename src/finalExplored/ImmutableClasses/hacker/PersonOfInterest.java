92d47b6538019cfd556d898375d2b60cc9ba5927
Miroslav Doan <Slithercze@gmail.com>
2024-03-19 22:33:12 +0100

    declaring immutable classes to produce immutable objects

---
+++ src/finalExplored/ImmutableClasses/hacker/PersonOfInterest.java
@@ -0,0 +1,15 @@
+package finalExplored.ImmutableClasses.hacker;
+
+import finalExplored.ImmutableClasses.PersonImmutable;
+
+public class PersonOfInterest extends PersonImmutable {
+
+    public PersonOfInterest(PersonImmutable person) {
+        super(person);
+    }
+
+//    @Override
+//    public PersonImmutable[] getKids() {
+//        return super.kids;
+//    }
+}
