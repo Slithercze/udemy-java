c4019c1d28a343cdf9956fb82d08a630b754da4c
Miroslav Doan <Slithercze@gmail.com>
2024-04-02 23:33:07 +0200

    file walking tree challenge & reading from file

---
+++ src/FilesIO/readingFiles/Main.java
@@ -0,0 +1,30 @@
+package FilesIO.readingFiles;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try (FileReader reader = new FileReader("src/FilesIO/readingFiles/file.txt")) {
+            char[] block = new char[1000];
+            int data;
+            while ((data = reader.read(block)) != -1) {
+                String content = new String(block, 0, data);
+                System.out.printf("---> [%d chars] %s%n", data, content);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("--------------------");
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("src/FilesIO/readingFiles/file.txt"))) {
+           bufferedReader.lines().forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
