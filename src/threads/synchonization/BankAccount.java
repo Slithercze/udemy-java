f89934204eb602c214f1ca7d0e839a8acb5bcb82
Miroslav Doan <Slithercze@gmail.com>
2024-04-30 23:47:12 +0200

    synchronized blocks

--- src/threads/synchonization/BankAccount.java
+++ src/threads/synchonization/BankAccount.java
@@ -3,9 +3,25 @@ package threads.synchonization;
 public class BankAccount {
 
     private double balance;
+    private String name;
 
-    public BankAccount(double balance) {
+    private final Object lockName = new Object();
+    private final Object lockBalance = new Object();
+
+    public BankAccount(String name, double balance) {
         this.balance = balance;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        synchronized (lockName) {
+            this.name = name;
+            System.out.println("Updated name = " + this.name);
+        }
     }
 
     public double getBalance() {
@@ -22,10 +38,21 @@ public class BankAccount {
             throw new RuntimeException(e);
         }
 
-        synchronized (this) {
+        synchronized (lockBalance) {
             double origBalance = balance;
             balance += amount;
             System.out.printf("STARTING BALANCE: %.0f, DEPOSIT (%.0f) : NEW BALANCE = %.0f%n", origBalance, amount, balance);
+            addPromoDollars(amount);
+        }
+    }
+
+    private void addPromoDollars(double amount) {
+
+        if (amount >= 5000) {
+            synchronized (lockBalance) {
+                System.out.println("Congratulations, you earned a promotional deposit.");
+                balance += 25;
+            }
         }
     }
 
