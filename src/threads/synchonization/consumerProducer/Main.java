4b3f946936a42627d2a99895c5591ea2d03c9c94
Miroslav Doan <Slithercze@gmail.com>
2024-05-20 23:14:02 +0200

    util.concurrent.locks

--- src/threads/synchonization/consumerProducer/Main.java
+++ src/threads/synchonization/consumerProducer/Main.java
@@ -1,37 +1,63 @@
 package threads.synchonization.consumerProducer;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class MessageRepository {
 
     private String message;
     private boolean hasMessage = false;
 
-    public synchronized String read() {
+    private final Lock lock = new ReentrantLock();
 
-        while (!hasMessage){
+    public String read() {
+
+        if (lock.tryLock()) {
             try {
-                wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                while (!hasMessage) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                hasMessage = false;
+            } finally {
+                lock.unlock();
             }
+        } else {
+            System.out.println("** read blocked " + lock);
+            hasMessage = false;
         }
-        hasMessage = false;
-        notifyAll();
         return message;
     }
 
-    public synchronized void write (String message) {
 
-        while (hasMessage){
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+    public void write(String message) {
+
+        try {
+            if (lock.tryLock(3, TimeUnit.SECONDS)) {
+                try {
+                    while (hasMessage) {
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    hasMessage = true;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("** write blocked " + lock);
+                hasMessage = true;
             }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        hasMessage = true;
-        notifyAll();
         this.message = message;
     }
 }
@@ -60,7 +86,7 @@ class MessageWriter implements Runnable {
             outgoingMessage.write(lines[i]);
             try {
                 Thread.sleep(random.nextInt(500, 2000));
-            } catch (InterruptedException e){
+            } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
@@ -86,7 +112,7 @@ class MessageReader implements Runnable {
         do {
             try {
                 Thread.sleep(random.nextInt(500, 2000));
-            } catch (InterruptedException e){
+            } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
             latestMessage = incomingMessage.read();
@@ -101,8 +127,24 @@ public class Main {
 
         MessageRepository messageRepository = new MessageRepository();
 
-        Thread reader = new Thread(new MessageReader(messageRepository));
-        Thread writer = new Thread(new MessageReader(messageRepository));
+        Thread reader = new Thread(new MessageReader(messageRepository), "Reader");
+        Thread writer = new Thread(new MessageWriter(messageRepository), "Writer");
+
+        writer.setUncaughtExceptionHandler((thread, exc) -> {
+            System.out.println("Writer had exception: " + exc);
+            if (reader.isAlive()) {
+                System.out.println("Going to interrupt the reader");
+                reader.interrupt();
+            }
+        });
+
+        reader.setUncaughtExceptionHandler((thread, exc) -> {
+            System.out.println("Reader had exception: " + exc);
+            if (writer.isAlive()) {
+                System.out.println("Going to interrupt the writer");
+                writer.interrupt();
+            }
+        });
 
         reader.start();
         writer.start();
