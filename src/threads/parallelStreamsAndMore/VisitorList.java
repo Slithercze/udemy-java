fd19297361f88199cd269a95e21d6da2d5c4da5b
Miroslav Doan <Slithercze@gmail.com>
2024-06-18 22:56:26 +0200

    ArrayBlockingQueue consumer

--- src/threads/parallelStreamsAndMore/VisitorList.java
+++ src/threads/parallelStreamsAndMore/VisitorList.java
@@ -6,21 +6,28 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
+import java.util.stream.Stream;
 
 public class VisitorList {
 
-    private static final ArrayBlockingQueue<Person> newVisitors = new ArrayBlockingQueue<>(5);
+    private static final CopyOnWriteArrayList<Person> masterList; //better if shared data is only for reading
+    private static final ArrayBlockingQueue<Person> newVisitors = new ArrayBlockingQueue<>(5); //better for control and manage the shared data being processed
 
+    static {
+        masterList = Stream.generate(Person::new)
+                .distinct()
+                .limit(2500)
+                .collect(CopyOnWriteArrayList::new,
+                        CopyOnWriteArrayList::add,
+                        CopyOnWriteArrayList::addAll);
+    }
     public static void main(String[] args) {
 
         Runnable producer = () -> {
 
             Person visitor = new Person();
-            System.out.println("Adding " + visitor);
+            System.out.println("Queueing " + visitor);
             boolean queued = false;
             try {
                 queued = newVisitors.offer(visitor, 5, TimeUnit.SECONDS);
@@ -28,7 +35,7 @@ public class VisitorList {
                 System.out.println("Illegal State Exception!");
             }
             if (queued) {
-                System.out.println(newVisitors);
+//                System.out.println(newVisitors);
             } else {
                 System.out.println("Queue is Full, cannot add " + visitor);
                 System.out.println("Draining Queue and writing data to file");
@@ -46,17 +53,51 @@ public class VisitorList {
             }
         };
 
+        Runnable consumer = () -> {
+            String threadName = Thread.currentThread().getName();
+            System.out.println(threadName + " Polling queue " + newVisitors.size());
+            Person visitor = null;
+            try {
+                visitor = newVisitors.take();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if (visitor != null) {
+                System.out.println(threadName + " " + visitor);
+                if (!masterList.contains(visitor)) {
+                    masterList.add(visitor);
+                    System.out.println("--> New Visitor gets Coupon!: " + visitor);
+                }
+            }
+            System.out.println(threadName + " done " + newVisitors.size());
+        };
+
         ScheduledExecutorService producerExecutor = Executors.newSingleThreadScheduledExecutor();
-        producerExecutor.scheduleWithFixedDelay(producer, 0, 1, TimeUnit.SECONDS);
+        producerExecutor.scheduleWithFixedDelay(producer, 0, 3, TimeUnit.SECONDS);
+
+        ScheduledExecutorService consumerPool = Executors.newScheduledThreadPool(3);
+        for (int i = 0; i < 3; i++) {
+            consumerPool.scheduleAtFixedRate(consumer, 6, 1, TimeUnit.SECONDS);
+        }
 
         while (true) {
             try {
-                if (!producerExecutor.awaitTermination(20, TimeUnit.SECONDS)) //20 vterin na to dokoncit thread pak se ukonci automaticky
+                if (!producerExecutor.awaitTermination(10, TimeUnit.SECONDS)) //20 vterin na to dokoncit thread pak se ukonci automaticky
                     break;
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }
         producerExecutor.shutdown();
+
+        while (true) {
+            try {
+                if (!consumerPool.awaitTermination(3, TimeUnit.SECONDS)) //20 vterin na to dokoncit thread pak se ukonci automaticky
+                    break;
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        consumerPool.shutdown();
     }
 }
