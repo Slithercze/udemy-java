cef68bc486fe3c56fa1e857e51cc8ad9280e5fe4
Miroslav Doan <Slithercze@gmail.com>
2024-09-09 22:36:25 +0200

    jpa, entity manager, jpql, criteria builder, native query + challenge and joins

---
+++ src/databases/JPA/Main.java
@@ -0,0 +1,27 @@
+package databases.JPA;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try (var sessionFactory = Persistence.createEntityManagerFactory(
+                "dev.lpa.music");
+             EntityManager entityManager = sessionFactory.createEntityManager();
+        ){
+            var transaction = entityManager.getTransaction();
+            transaction.begin();
+            Artist artist = entityManager.find(Artist.class, 103);
+            System.out.println(artist);
+//            artist.addAlbum("The Best of Muddy Waters");
+//            System.out.println(artist);
+//            entityManager.remove(artist);
+//            entityManager.persist(new Artist("Muddy Water"));
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
